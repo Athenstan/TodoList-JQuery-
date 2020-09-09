7d3164ba9b4f6d7c3b419151424e614e17a42ae1
Athenstan <athenstan5@gmail.com>
2020-09-08 18:15:36 -0700

    all functionality has been added and needs styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,11 @@
-// check off specific todos by clicking 
 
-$("li").click(function(){
-$(this).toggleClass("completed");
+
+
+// check off specific todos by clicking 
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+
+// Refactored into line above ^ 
 //     // if li is grey, turn it black, if black turn grey 
 // if($(this).css("color") === "rgb(128, 128, 128)"){
 //     $(this).css({
@@ -19,3 +23,26 @@ $(this).toggleClass("completed");
 // }
 
 });
+
+// Delete Todos by clicking on the X: 
+ $("ul").on("click", "span", function(event){
+     $(this).parent().fadeOut(500, function(){
+         $(this).remove();
+     }); 
+     event.stopPropagation(); 
+    
+ });
+
+ // Add input when the enter key is pressed in the input box 
+
+ $("input[type='text'").keypress(function(event){
+     if (event.which === 13){
+         // Taking text from input
+         var textvalue = $(this).val(); 
+         //Clear the value in the textbox 
+         $(this).val(""); 
+         // Create a new li and add to ul 
+         $("ul").append("<li><span> x </span>"  + textvalue + "</li>"); 
+         
+     }
+ }); 
